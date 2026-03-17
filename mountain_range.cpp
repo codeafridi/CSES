0ace42e5c66cabe332cf489a2762756868e6ff47
Mohammed Afridi <code.afridi@gmail.com>
2026-03-18 00:10:30 +0530

    maintain range

--- mountain_range.cpp
+++ mountain_range.cpp
@@ -1,60 +1,36 @@
-// #include<bits/stdc++.h>
-// using namespace std;
-// typedef long long ll;
-// int main(){
-//     ll n;
-//     cin >> n;
-//     vector<ll> v(n);
-//     for(ll i = 0; i<n; i ++){
-//         cin >> v[i];
-//     }
-//     ll  temp = 0;
-    
-//     for(ll i = 0; i<n; i ++){
-//         ll count = 1;
-//         for(ll j=i-1 ; j>=0 ; j--){
-//             if(v[j] < v[i]) count ++;
-//             else break;
-//         }
-//         for(ll j = i+1 ; j<n ; j++){
-//             if(v[j] < v[i]) count ++;
-//             else break;
-//         }
-//         temp = max(count,temp);
-//     }
-//    cout << temp;
-    
-//     return 0;
-// } 
 #include <bits/stdc++.h>
 using namespace std;
 
 int main(){
-    long long n;
+    int n;
     cin >> n;
 
-    vector<long long> v(n);
-    for(long long i = 0; i < n; i++){
-        cin >> v[i];
-    }
+    vector<int> v(n);
+    for(int i = 0; i < n; i++) cin >> v[i];
 
-    long long temp = 0;
+    int ans = 0;
 
-    for(long long i = 0; i < n; i++){
-        long long count = 1;
+    for(int i = 0; i < n; i++){
+        int count = 1;
 
-        for(long long j = i-1; j >= 0; j--){
-            if(v[j] < v[i]) count++;
+        // go left
+        int mx = 0;
+        for(int j = i-1; j >= 0; j--){
+            mx = max(mx, v[j]);
+            if(mx < v[i]) count++;
             else break;
         }
 
-        for(long long j = i+1; j < n; j++){
-            if(v[j] < v[i]) count++;
+        // go right
+        mx = 0;
+        for(int j = i+1; j < n; j++){
+            mx = max(mx, v[j]);
+            if(mx < v[i]) count++;
             else break;
         }
 
-        temp = max(count, temp); // ✅ FIXED
+        ans = max(ans, count);
     }
 
-    cout << temp;
+    cout << ans;
 }
