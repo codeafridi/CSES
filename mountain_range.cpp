955ff6c2bd715d758f0244d0e002b5181d1669c2
Mohammed Afridi <code.afridi@gmail.com>
2026-03-18 00:00:39 +0530

    maintain range

--- mountain_range.cpp
+++ mountain_range.cpp
@@ -1,28 +1,60 @@
-#include<bits/stdc++.h>
+// #include<bits/stdc++.h>
+// using namespace std;
+// typedef long long ll;
+// int main(){
+//     ll n;
+//     cin >> n;
+//     vector<ll> v(n);
+//     for(ll i = 0; i<n; i ++){
+//         cin >> v[i];
+//     }
+//     ll  temp = 0;
+    
+//     for(ll i = 0; i<n; i ++){
+//         ll count = 1;
+//         for(ll j=i-1 ; j>=0 ; j--){
+//             if(v[j] < v[i]) count ++;
+//             else break;
+//         }
+//         for(ll j = i+1 ; j<n ; j++){
+//             if(v[j] < v[i]) count ++;
+//             else break;
+//         }
+//         temp = max(count,temp);
+//     }
+//    cout << temp;
+    
+//     return 0;
+// } 
+#include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
+
 int main(){
-    ll n;
+    long long n;
     cin >> n;
-    vector<ll> v(n);
-    for(ll i = 0; i<n; i ++){
+
+    vector<long long> v(n);
+    for(long long i = 0; i < n; i++){
         cin >> v[i];
     }
-    ll  temp = 0;
-    
-    for(ll i = 0; i<n; i ++){
-        ll count = 1;
-        for(ll j=i-1 ; j>=0 ; j--){
-            if(v[j] < v[i]) count ++;
+
+    long long temp = 0;
+
+    for(long long i = 0; i < n; i++){
+        long long count = 1;
+
+        for(long long j = i-1; j >= 0; j--){
+            if(v[j] < v[i]) count++;
             else break;
         }
-        for(ll j = i+1 ; j<n ; j++){
-            if(v[j] < v[i]) count ++;
+
+        for(long long j = i+1; j < n; j++){
+            if(v[j] < v[i]) count++;
             else break;
         }
-        temp = max(count,temp);
+
+        temp = max(count, temp); // ✅ FIXED
     }
-   cout << temp;
-    
-    return 0;
-} 
+
+    cout << temp;
+}
