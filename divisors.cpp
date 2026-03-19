56671bed6d2c51027ae1e6736a1e195d5f2bce7e
Mohammed Afridi <code.afridi@gmail.com>
2026-03-19 13:23:53 +0530

    divisor

--- divisors.cpp
+++ divisors.cpp
@@ -1,22 +1,54 @@
-#include<bits/stdc++.h>
+// #include<bits/stdc++.h>
+// using namespace std;
+// typedef long long ll;
+// int main(){
+//     ios::sync_with_stdio(false);
+//     cin.tie(NULL);
+//     ll n;
+//     cin >> n;
+//     vector<ll> v(n);
+//     for(ll i = 0;i <n; i++){
+//         cin >> v[i];
+//     }
+//     ll count=0;
+//     for(ll i = 0;i<(ll)v.size();i++){
+//         for(ll j = 1; j<=v[i]; j++){
+//             if(v[i]%j == 0) count++;
+//         }
+//         cout<<count<<"\n";
+//         count=0;
+//     }
+//     return 0;
+// }
+
+#include <bits/stdc++.h>
 using namespace std;
+
 typedef long long ll;
+
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
+   
+
     ll n;
     cin >> n;
+
     vector<ll> v(n);
-    for(ll i = 0;i <n; i++){
+    for(ll i = 0; i < n; i++){
         cin >> v[i];
     }
-    ll count=0;
-    for(ll i = 0;i<(ll)v.size();i++){
-        for(ll j = 1; j<=v[i]; j++){
-            if(v[i]%j == 0) count++;
+
+    for(ll i = 0; i < n; i++){
+        ll count = 0;
+
+        for(ll j = 1; j*j <= v[i]; j++){
+            if(v[i] % j == 0){
+                count++;
+                if(j != v[i]/j) count++;
+            }
         }
-        cout<<count<<"\n";
-        count=0;
+
+        cout << count << "\n";
     }
+
     return 0;
 }
