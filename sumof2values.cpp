0a5cf5d97a612f4f1139839bf87a7d66321873fd
Mohammed Afridi <code.afridi@gmail.com>
2026-03-23 18:06:46 +0530

    sumof2values

---
+++ sumof2values.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main(){
+    ll n,x;
+    cin >> n >> x;
+    ll arr[n];
+    for(ll i =0 ; i<n; i++){
+        cin >> arr[i];
+    }
+    sort(arr,arr+n);
+    for(ll i =0 ; i<n; i++){
+        for(ll j =i+1 ; j<n; j++){
+            if(arr[i]+arr[j] == x){
+                cout << i+1 << " " << j+1;
+                return 0;
+            }
+        }
+    }
+    cout << "IMPOSSIBLE";
+    return 0;
+}
