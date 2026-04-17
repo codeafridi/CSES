e6d0390187100629bb73a51695a42ff593587427
Mohammed Afridi <code.afridi@gmail.com>
2026-04-17 21:22:39 +0530

    changes

---
+++ Way_Too_Long_Words.cpp
@@ -0,0 +1,44 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin >>n;
+//     vector<string> v(n);
+//     for(int i=0;i<n;i++){
+//         cin >>v[i];
+//     }
+//     for(int i =0;i<n;i++){
+//         if(v[i].size() > 10){
+//             cout<<v[i][0]<<(v[i].size()-2)<< v[i][(v[i].size() -1)] << "\n";
+//         }
+//         else {
+//             cout<<v[i]<<"\n";
+//         }
+//         return 0;
+//     }
+// }
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<string> v(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (v[i].size() > 10) {
+            cout << v[i][0] << (v[i].size() - 2)
+                 << v[i][v[i].size() - 1] << "\n";
+        } else {
+            cout << v[i] << "\n";
+        }
+    }
+
+    return 0;
+}
