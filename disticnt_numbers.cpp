58603d0cd4c33ec95603f27a9ca0cf4c5e5054c5
codeafridi <code.afridi@gmail.com>
2026-05-07 14:24:56 +0530

    changed code

--- disticnt_numbers.cpp
+++ disticnt_numbers.cpp
@@ -13,23 +13,22 @@ Output:
 
 */
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-int main(){
-    ll n;
+
+int main() {
+    long long n;
     cin >> n;
-    vector<ll> v(n);
-    for(ll i = 0; i < n ; i++){
-        cin >> v[i];
+
+    vector<long long> v(n);
+    for(int i = 0; i < n; i++) cin >> v[i];
+
+    sort(v.begin(), v.end());
+
+    long long count = 1;
+    for(int i = 1; i < n; i++){
+        if(v[i] != v[i-1]) count++;
     }
-    sort(v.begin(),v.end());
-    ll count = 0;
-    for(ll i = 0; i<n ; i ++){
-        if(v[i] != v[i+1]) count++;
-    }
+
     cout << count;
-    return 0;
-
-
 }
