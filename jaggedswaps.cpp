93d5d5a88bd26827a22cdbe7d2ea4984ed4bb7fd
codeafridi <code.afridi@gmail.com>
2026-05-22 16:21:01 +0530

    swap

---
+++ jaggedswaps.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#include <utility>
+using namespace std;
+typedef long long ll;
+int main(){
+    ll t;
+    cin >> t;
+    while(t--){
+        ll n;
+        cin >> n;
+        vector<ll> v(n);
+        for(ll i= 0; i<n ; i++){
+            cin >> v[i];
+        }
+        if(v[0] == 1){
+            cout << "YES" << endl;
+        }
+        else{
+            cout << "NO" << endl;
+        }   
+    }
+    return 0;
+}
