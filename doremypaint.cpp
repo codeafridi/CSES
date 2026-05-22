2a37adf424c35d3f3a80def5d343e3570e7ede0f
codeafridi <code.afridi@gmail.com>
2026-05-22 18:19:10 +0530

    removed indenstation

--- doremypaint.cpp
+++ doremypaint.cpp
@@ -25,10 +25,10 @@ int main(){
         if(freq1 == freq2){
             cout<<"yes"<<endl;
     }
-    else if(n%2 == 0 && abs(freq1 - freq2) == 1){
+        else if(n%2 == 0 && abs(freq1 - freq2) == 1){
         cout<<"yes"<< endl;
     }
-    else{
+        else{
         cout<<"no"<<endl;
     }
 }
