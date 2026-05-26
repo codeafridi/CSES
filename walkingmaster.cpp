64f245617468a5af43d0e8153b5638abf112a131
codeafridi <code.afridi@gmail.com>
2026-05-26 18:28:07 +0530

    walking master

---
+++ walkingmaster.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while(t--) {
+        long long a, b, c, d;
+        cin >> a >> b >> c >> d;
+
+        long long k = d - b;
+
+        // y cannot decrease
+        if(k < 0) {
+            cout << -1 << endl;
+            continue;
+        }
+
+        // after k diagonal moves
+        long long x = a + k;
+
+        // cannot increase x anymore
+        if(c > x) {
+            cout << -1 << endl;
+            continue;
+        }
+
+        // left moves needed
+        long long left = x - c;
+
+        cout << k + left << endl;
+    }
+}
